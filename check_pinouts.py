ecbdbfef6a2796733cebfb395762ef8b1d80abf9
Andrew Peck <anp@ucla.edu>
2017-06-08 11:56:19 -0700

    Further development of scripts, addition of constraints generation

--- check_pinouts.py
+++ check_pinouts.py
@@ -1,5 +1,10 @@
-from alct_pinouts import *
 
+from alct_pinouts    import *
+from name_translator import *
+from led_lut         import *
+
+
+# export netlist as  MultiWire
 
 def parse_netlist():
     filename = 'FPGA_IO.NET'
@@ -11,7 +16,8 @@ def parse_netlist():
 
         if (line[0]!='-'):
             (net,part,pin) = line.split()
-            netlist.update({net: pin})
+            if (part=='U1' and net[0]!='+' and net!='GND'):
+                netlist.update({net: pin})
 
 #    for key in netlist:
 #        print netlist[key]
@@ -22,22 +28,11 @@ def check_ios (io_list, netlist):
     for key in io_list:
         value = io_list [key]
         #print value[0:2]
-        if (value=='GND'):
-            #print "Ground"
+
+        if (not pin_needs_ucf(value)):
             pass
-        elif (value[0:2]=='3v3'[0:2]):
-            #print "3.3v"
-            pass
-        elif (value[0:2]=='1v8'[0:2]):
-            #print "3.3v"
-            pass
-        elif (value[0]=='+'[0]):
-            #print "3.3v"
-            pass
-        elif (value[0:1] == "NC"[0:1]):
-            #print "no connect"
-            pass
-        elif (value[0:1] == "tp"[0:1]):
+
+        if (value[0:2] == "tp"):
             a = netlist.get(value, "not found")
             if (a=="not found"):
                 print key + "\t\t" + "\t\t" + value + "\t\t" +  a
@@ -50,9 +45,107 @@ def check_ios (io_list, netlist):
     print ''
     print ''
 
+
+def pin_needs_ucf (io_signal):
+    if (io_signal=='GND'):
+        return False
+    elif (io_signal[0:3]=='3v3'):
+        return False
+    elif (io_signal[0:3]=='1v8'):
+        return False
+    elif (io_signal[0]=='+'):
+        return False
+    elif (io_signal[0:2] == "NC"):
+        return False
+    elif (io_signal[4:9] == "prgrm"):
+        return False
+    elif (io_signal == '/hard_rst'):
+        return False
+
+    return True
+
+
+def gen_constraints (filename, samtec_io, base_led, netlist):
+
+    f = open(filename, 'w')
+
+    # loop over ALCT Baseboard Samtec IOs (XPXX_YY)
+
+    for io in samtec_io:
+
+        # io       = XPXX_YY
+        # signal   = lct2_8  (for example)
+        # ucf_name = lct2<8> (for example)
+
+        signal    = samtec_io [io]
+
+        # power and ground and etc pins etc don't need to be added to the ucf
+        if (pin_needs_ucf(signal)):
+
+            ucf_name = name_lut    [signal];
+
+            if   (signal[0:2] == "tp" and signal[2]!='_'):
+                pin = netlist.get(signal, "not found")
+            elif (signal=="clock"):
+                pin = netlist.get("mez_clock", "not found")
+            elif (base_led.get(signal, "not found")!='not found'):
+                pin = netlist.get(base_led.get(signal), "not found")
+            else:
+                pin = netlist.get (io, "not found");
+
+            #print '{:20s}  {:20s}  {:20s}  {:20s}'.format(io, signal, ucf_name, pin)
+            constraint = 'NET {:20s} LOC = {:10s} # {:10s} {:20s}'.format('"' + ucf_name + '"', '"'+pin+'";' , io, signal);
+            print constraint
+            f.write(constraint + "\n")
+            #print 'NET ' + '{:20s}'.format(ucf_name) + '"' + '     LOC = '
+
+    f.write ('#------------------------------------------------------------------------\n')
+    f.write ('# IOB Modifiers                                                          \n')
+    f.write ('#------------------------------------------------------------------------\n')
+    f.write ('NET "clk80<*>"            SLEW=FAST | DRIVE=12;                          \n')
+    f.write ('                                                                         \n')
+    f.write ('NET "rsrvd_out<*>"        SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "ext_inject_trig"     SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "activeFeb_cfgDone"   SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "valid"               SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "keyp<*>"             SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "quality<*>"          SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "amu"                 SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "bxn_wrFifo<*>"       SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "L1A_SyncAdb"         SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "daqData<*>"          SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "lctSpec_FirstFr"     SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "dduSpec_LastFr"      SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('NET "seq_seu<*>"          SLEW=SLOW | DRIVE=12;                          \n')
+    f.write ('                                                                         \n')
+    f.write ('#------------------------------------------------------------------------\n')
+    f.write ('# Time Specs                                                             \n')
+    f.write ('#------------------------------------------------------------------------\n')
+    f.write ('NET "tck2" TNM_NET = "tck2";                                             \n')
+    f.write ('TIMESPEC "TS_tck2" = PERIOD "tck2" 21 ns HIGH 50 %;                      \n')
+    f.write ('NET "clkp" TNM_NET = "clkp";                                             \n')
+    f.write ('TIMESPEC "TS_clkp" = PERIOD "clkp" 21 ns HIGH 50 %;                      \n')
+    f.write ('                                                                         \n')
+    f.write ('NET "tck2" CLOCK_DEDICATED_ROUTE = FALSE;                                \n')
+
+#for led in base_led:
+#
+#
+#    if (signal != 'nc'):
+#
+#        ucf_name = name_lut    [signal];
+#        pin      = netlist.get (led, "not found");
+#        print '{:20s}  {:20s}  {:20s}  {:20s}'.format(io, signal, ucf_name, pin)
+
+
+
+#check_ios(a288_io, netlist);
+#check_ios(a384_io, netlist);
+#check_ios(a672_io, netlist);
+
 netlist = parse_netlist()
 
-check_ios(a288_io, netlist);
-check_ios(a384_io, netlist);
-check_ios(a672_io, netlist);
+gen_constraints ("alct288.ucf", a288_io, led_288, netlist);
+gen_constraints ("alct384.ucf", a384_io, led_384, netlist);
+gen_constraints ("alct672.ucf", a672_io, led_672, netlist);
 
---
+++ led_lut.py
@@ -0,0 +1,82 @@
+led_288 = {
+    'jstate0_grn' : 'base_led1',
+    'feb0_grn'    : 'base_led2',
+    'feb4_grn'    : 'base_led3',
+    'tdi_grn'     : 'base_led4',
+
+    'jstate1_grn' : 'base_led5',
+    'feb1_grn'    : 'base_led6',
+    'feb5_grn'    : 'base_led7',
+    'tck_grn'     : 'base_led8',
+    'invpat_red'  : 'base_led9',
+    'pretrig_blu' : 'base_led10',
+
+    'jstate2_grn' : 'base_led11',
+    'feb2_grn'    : 'base_led12',
+    'feb6_grn'    : 'base_led13',
+    'tms_grn'     : 'base_led14',
+    'halt_red'    : 'base_led15',
+
+    'jstate3_grn' : 'base_led16',
+    'feb3_grn'    : 'base_led17',
+    'tdo_grn'     : 'base_led18',
+    'l1a_ok_grn'  : 'base_led19',
+    'no_l1a_red'  : 'base_led20'
+}
+
+led_384 = {
+
+    'feb1_grn'    : 'base_led1',
+    'feb5_grn'    : 'base_led2',
+    'tck_grn'     : 'base_led3',
+    'invpat_red'  : 'base_led2',
+
+    'feb0_grn'    : 'base_led5',
+    'feb4_grn'    : 'base_led6',
+    'tms_grn'     : 'base_led7',
+    'amu_grn'     : 'base_led8',
+    'pretrig_blu' : 'base_led9',
+
+    #  'nc'       : 'base_led10',
+
+    'jstate3_grn' : 'base_led11',
+    'feb3_grn'    : 'base_led12',
+    'tdo_grn'     : 'base_led13',
+    'halt_red'    : 'base_led14',
+    'l1a_ok_grn'  : 'base_led15',
+
+    'jstate2_grn' : 'base_led16',
+    'feb2_grn'    : 'base_led17',
+    'feb6_grn'    : 'base_led18',
+    'tdi_grn'     : 'base_led19',
+    'no_l1a_red'  : 'base_led20'
+
+}
+
+led_672 = {
+
+    'jstate0_grn' : 'base_led1',
+    'feb0_grn'    : 'base_led2',
+    'feb4_grn'    : 'base_led3',
+    'tdi_grn'     : 'base_led4',
+
+    'jstate1_grn' : 'base_led5',
+    'feb1_grn'    : 'base_led6',
+    'feb5_grn'    : 'base_led7',
+    'tck_grn'     : 'base_led8',
+    'invpat_red'  : 'base_led9',
+    'pretrig_blu' : 'base_led10',
+
+    'jstate2_grn' : 'base_led11',
+    'feb2_grn'    : 'base_led12',
+    'feb6_grn'    : 'base_led13',
+    'tms_grn'     : 'base_led14',
+    'halt_red'    : 'base_led15',
+
+    'jstate3_grn' : 'base_led16',
+    'feb3_grn'    : 'base_led17',
+    'tdo_grn'     : 'base_led18',
+    'l1a_ok_grn'  : 'base_led19',
+    'no_l1a_red'  : 'base_led20'
+
+}
